2b636119dbdfbfe3f4868b5da0c0ae817883f459
AKRITI-ENG <kumariakritiyd@gmail.com>
2026-04-15 14:05:00 +0530

    feat: add reusable debounce hook

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useDebounce(value, delay = 500) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [delay, value]);
+
+  return debouncedValue;
+}
+
+export default useDebounce;
